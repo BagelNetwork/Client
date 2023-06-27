921979a51c2a921d8a054e7e7ec40a3fa7a788c1
Bidhan Roy <mail2bidhan@gmail.com>
2023-06-27 17:17:43 +0600

    documentation

--- BagelDB.js
+++ BagelDB.js
@@ -1,12 +1,24 @@
 const axios = require('axios');
 
+/**
+ * BagelDB is a class to interact with the BagelDB and OpenAI APIs.
+ * It provides methods to insert data, search data in BagelDB, and get embeddings from OpenAI.
+ * This class requires the axios module and uses environment variables.
+ */
 class BagelDB {
+  /**
+   * Constructor to initialize BagelDB and OpenAI base URLs and OpenAI API key from environment variables.
+   */
   constructor() {
     this.baseURL = 'https://api.bageldb.ai';
     this.openAIURL = 'https://api.openai.com';
     this.openAIKey = process.env.OPENAI_API_KEY;
   }
 
+  /**
+   * Async method to send a ping request to BagelDB API.
+   * @returns {Promise} A promise that resolves with the response from the BagelDB API.
+   */
   async ping() {
     try {
       const response = await axios.get(`${this.baseURL}/v0/ping`);
@@ -16,6 +28,13 @@ class BagelDB {
     }
   }
 
+  /**
+   * Async method to get OpenAI embeddings.
+   * @param {string} inputText - The input text for which embeddings are required.
+   * @param {string} [model='text-embedding-ada-002'] - The model to use for generating embeddings. Defaults to 'text-embedding-ada-002'.
+   * @returns {Promise} A promise that resolves with the embeddings from OpenAI.
+   * @throws Will throw an error if inputText is not a string or if OpenAI API key is missing from environment variables.
+   */
   async getOpenAIEmbedding(inputText, model='text-embedding-ada-002') {
     if (typeof inputText !== 'string') {
         throw new Error("Input text must be a string");
@@ -41,6 +60,13 @@ class BagelDB {
     }
   }
 
+  /**
+   * Async method to insert vectors into a given index in BagelDB.
+   * @param {string} index - The index in which vectors are to be inserted.
+   * @param {Array} vectors - An array of vectors to be inserted.
+   * @returns {Promise} A promise that resolves with the response from the BagelDB API.
+   * @throws Will throw an error if index is not a string, vectors is not an array, or if each vector is not an object with an 'id', 'values' array, and 'metadata' object.
+   */
   async insert(index, vectors) {
     if (typeof index !== 'string') {
       throw new Error("Index must be a string");
@@ -65,6 +91,13 @@ class BagelDB {
     }   
   }
 
+  /**
+   * Async method to search for a vector in a given index in BagelDB.
+   * @param {string} index - The index in which the search is to be performed.
+   * @param {Array} vector - The vector for which the search is to be performed.
+   * @returns {Promise} A promise that resolves with the response from the BagelDB API.
+   * @throws Will throw an error if index is not a string or vector is not an array.
+   */
   async search(index, vector) {
     if (typeof index !== 'string') {
       throw new Error("Index must be a string");
