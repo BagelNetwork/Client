076b4e3af2beaa5c520808a57fff6169f8490b35
Bidhan Roy <mail2bidhan@gmail.com>
2023-08-06 23:14:28 +0300

    0.2.32

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="betabageldb",
-    version="0.2.31",
+    version="0.2.32",
     description="BagelDB is a Python library for interacting with the BagelDB API.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
