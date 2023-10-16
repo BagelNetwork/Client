93e9cecde5a2f5da77ae24d222d948a312bc4b33
Nurul Akter Towhid <nurulaktertowhid@gmail.com>
2023-10-17 01:28:55 +0600

    Unit test (#38)
    * add test code
    
    * test_upsert

---
+++ python/test/client_test.py
@@ -0,0 +1,70 @@
+import pytest
+import uuid
+import bagel
+from bagel.config import Settings
+
+
+CLUSTER_NAME = str(uuid.uuid4())
+
+
+@pytest.fixture
+def cluster():
+    server_settings = Settings(
+        bagel_api_impl="rest",
+        bagel_server_host="api.bageldb.ai",
+    )
+
+    client = bagel.Client(server_settings)
+    return client.get_or_create_cluster(CLUSTER_NAME)
+
+
+def test_cluster(cluster):
+    assert cluster.cluster_size == 0
+    assert cluster.embedding_size is None
+
+
+def test_add(cluster):
+    cluster.add(ids=["id"], documents=["test doc"], metadatas=[{"meta": "m1"}])
+    assert cluster.count() == 1
+
+
+def test_get(cluster):
+    resp = cluster.get(ids=["id"])
+    assert resp.get("ids")[0] == "id"
+
+
+def test_find(cluster):
+    resp = cluster.find(query_texts=["test"], n_results=1)
+    assert resp.get("ids")[0] == ["id"]
+
+
+def test_upsert(cluster):
+    cluster.upsert(
+        documents=[
+            "test new",
+            "test upsert",
+        ],
+        metadatas=[{"source": "notion"}, {"source": "google"}],
+        ids=["id", "id3"],
+    )
+    assert cluster.count() == 2
+
+
+def test_img_url(cluster):
+    url = [
+        "https://bagel-public-models-s3-download.s3.eu-north-1.amazonaws.com/dog/thumb-1920-454156.jpg",
+    ]
+    cluster.add_image_urls(ids=["ids"], urls=url)
+    assert cluster.count() == 3
+
+
+def test_delete():
+    server_settings = Settings(
+        bagel_api_impl="rest",
+        bagel_server_host="api.bageldb.ai",
+    )
+
+    client = bagel.Client(server_settings)
+    client.delete_cluster(CLUSTER_NAME)
+    with pytest.raises(Exception):
+        client.get_cluster(CLUSTER_NAME)
