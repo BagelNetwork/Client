49374508bea183b19f1ec7504d4762939e953070
mesbahtanvir <mesbah.tanvir.cs@gmail.com>
2023-10-28 01:06:22 -0400

    npx standard . --fix

--- javascript/BagelDB.js
+++ javascript/BagelDB.js
@@ -1,14 +1,13 @@
-const api = require('./src/api/api.js');
-const settigs = require('./src/utils/settings.js');
-const cluster = require('./src/model/cluster.js');
-
+const api = require('./src/api/api.js')
+const settigs = require('./src/utils/settings.js')
+const cluster = require('./src/model/cluster.js')
 
 // exports
 module.exports = {
-    // api
-    Client: api.API,
-    // utils
-    Settings: settigs.Settings,
-    // cluster
-    Cluster: cluster.Cluster,
-};
+  // api
+  Client: api.API,
+  // utils
+  Settings: settigs.Settings,
+  // cluster
+  Cluster: cluster.Cluster
+}
--- javascript/src/utils/settings.js
+++ javascript/src/utils/settings.js
@@ -1,48 +1,47 @@
-const config = require("./config.js");
+const config = require('./config.js')
 
 class Settings {
-    constructor(options = {}) {
-        // default params
-        this.environment = "";
-        this.bagel_api_impl = "bagel.api.fastapi.FastAPI";
-        this.clickhouse_host = null;
-        this.clickhouse_port = null;
-        this.persist_directory = ".bagel";
-        this.bagel_server_host = null;
-        this.bagel_server_http_port = null;
-        this.bagel_server_ssl_enabled = false;
-        this.bagel_server_grpc_port = null;
-        this.bagel_server_cors_allow_origins = [];
-        this.anonymized_telemetry = true;
-        this.allow_reset = false;
-        this.sqlite_database = ":memory:";
-        this.migrations = "apply";
+  constructor (options = {}) {
+    // default params
+    this.environment = ''
+    this.bagel_api_impl = 'bagel.api.fastapi.FastAPI'
+    this.clickhouse_host = null
+    this.clickhouse_port = null
+    this.persist_directory = '.bagel'
+    this.bagel_server_host = null
+    this.bagel_server_http_port = null
+    this.bagel_server_ssl_enabled = false
+    this.bagel_server_grpc_port = null
+    this.bagel_server_cors_allow_origins = []
+    this.anonymized_telemetry = true
+    this.allow_reset = false
+    this.sqlite_database = ':memory:'
+    this.migrations = 'apply'
 
-        // override defaults
-        Object.keys(options).forEach((key) => {
-            this[key] = options[key];
-        });
-    }
+    // override defaults
+    Object.keys(options).forEach((key) => {
+      this[key] = options[key]
+    })
+  }
 
-    require(key) {
-        const val = this[key];
-        if (val === undefined || val === null) {
-            throw new Error(`Missing required config value '${key}'`);
-        }
-        return val;
+  require (key) {
+    const val = this[key]
+    if (val === undefined || val === null) {
+      throw new Error(`Missing required config value '${key}'`)
     }
+    return val
+  }
 
-    // In JavaScript, we cannot directly access class attributes like in Python.
-    // Instead, we can define getter methods to achieve similar functionality.
-    get(key) {
-        const val = this[key];
-        // Backwards compatibility with short names instead of full class names
-        if (config.default.legacy_config_values[val]) {
-            return config.default.legacy_config_values[val];
-        }
-        return val;
+  // In JavaScript, we cannot directly access class attributes like in Python.
+  // Instead, we can define getter methods to achieve similar functionality.
+  get (key) {
+    const val = this[key]
+    // Backwards compatibility with short names instead of full class names
+    if (config.default.legacy_config_values[val]) {
+      return config.default.legacy_config_values[val]
     }
+    return val
+  }
 };
 
-
-module.exports = {Settings};
+module.exports = { Settings }
