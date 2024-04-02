8ba00466800cae2c16ffdff74a59b6f7c74f8106
ihpolash <ismailhossainpolash@gmail.com>
2024-04-02 23:56:21 +0600

    update header on upsert class

--- python/bagel/api/fastapi.py
+++ python/bagel/api/fastapi.py
@@ -390,10 +390,8 @@ class FastAPI(API):
         Updates a batch of embeddings in the database
         - pass in column oriented data lists
         """
-        if api_key:
-            headers = self._popuate_headers_with_api_key(api_key)
-        else:
-            headers = self.__headers
+        
+        headers = self._popuate_headers_with_api_key(api_key)
 
         resp = requests.post(
             self._api_url + "/clusters/" + str(cluster_id) + "/upsert",
