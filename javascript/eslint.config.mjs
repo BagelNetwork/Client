0438194ef286699e5e571bcb5ac313000d58b071
Bidhan Roy <mail2bidhan@gmail.com>
2024-08-05 13:40:36 -0400

    eslint

--- javascript/eslint.config.mjs
+++ javascript/eslint.config.mjs
@@ -2,6 +2,20 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 
 export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.jest  // Add Jest globals
+      }
+    },
+    ignores: ['bagelDB.test.js']  // Ignore bagelDB.test.js
+  },
+  pluginJs.configs.recommended,
+  {
+    files: ['**/*.js'],
+    rules: {
+      // You can add any specific rules here if needed
+    }
+  }
 ]
