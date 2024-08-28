65fe1989823eb4deebe8f12c7f0b78d22ace9ba1
Bidhan Roy <mail2bidhan@gmail.com>
2024-08-28 13:14:16 -0400

    PyPi upgrade

--- python/update.sh
+++ python/update.sh
@@ -20,5 +20,11 @@ pip install -r requirements.txt
 # Run setup.py to create a source distribution and built distribution
 python setup.py sdist bdist_wheel
 
-# Upload the distributions to PyPi
+# Prompt the user for the API token
+echo "Enter your PyPI API token:"
+read -s PYPI_API_TOKEN
+
+# Upload the distributions to PyPi using the provided API token
+export TWINE_USERNAME="__token__"
+export TWINE_PASSWORD="$PYPI_API_TOKEN"
 twine upload dist/*
